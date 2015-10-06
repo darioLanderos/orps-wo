085787530f6dce95c1d5bb442c0d57a1ae598f82
Dario Landeros <dlanderos@gmail.com>
2015-10-06 00:41:47 +0000

    Smart Search feature included in reports

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'devise'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
+  gem 'test-unit'
   
   # BootStrap
   # gem "therubyracer"
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,10 +3,11 @@ class ReportsController < ApplicationController
   before_filter :authenticate_user!
   
   def by_address
-    if params[:search]
-      @service_requests = ServiceRequest.find(:all, :conditions => ['address LIKE ?', "%#{params[:search]}%"])
+    if params[:show_closed]
+      @service_requests = ServiceRequest.where("status" => 2) #ServiceRequest.where("address LIKE ?", "%#{params[:search]}%") 
+      #ServiceRequest.find(:all, :conditions => ['address LIKE ?', "%#{params[:search]}%"])
     else
-      @service_requests = ServiceRequest.all
+      @service_requests = ServiceRequest.where("status != ?", 2) #ServiceRequest.all
     end
 
     respond_to do |format|
@@ -17,10 +18,11 @@ class ReportsController < ApplicationController
 
   def by_status
     if ((params[:search]) && (params[:search] != "0"))
-      @service_requests = ServiceRequest.find(:all, :conditions => ['status = ?', "#{params[:search]}"])
+      @service_requests = ServiceRequest.where("status = ?", "#{params[:search]}") 
+      #ServiceRequest.find(:all, :conditions => ['status = ?', "#{params[:search]}"])
       @received_params = params[:search]
     else
-      @service_requests = ServiceRequest.all
+      @service_requests = ServiceRequest.where("status = ?", 1) #ServiceRequest.all
       @received_params = "Nothing received"
     end
 
@@ -34,14 +36,16 @@ class ReportsController < ApplicationController
     
     if((params[:start_date]) && (params[:finish_date]))
       if ((params[:search])  && (params[:search] != "0"))
-        @service_requests = ServiceRequest.find(:all, :conditions => ['category_id = ? AND created_at between ? and ?', "#{params[:search]}","#{params[:start_date]}" ,"#{params[:finish_date]}"])
+        @service_requests = ServiceRequest.where("category_id = ? AND created_at between ? and ?", "#{params[:search]}","#{params[:start_date]}" ,"#{params[:finish_date]}") 
+        #ServiceRequest.find(:all, :conditions => ['category_id = ? AND created_at between ? and ?', "#{params[:search]}","#{params[:start_date]}" ,"#{params[:finish_date]}"])
         @received_params = params[:search]
       else
-        @service_requests = ServiceRequest.find(:all, :conditions => ['created_at between ? and ?', "#{params[:start_date]}" ,"#{params[:finish_date]}"])
+        @service_requests = ServiceRequest.where("created_at BETWEEN ? AND ?", "#{params[:start_date]}", "#{params[:finish_date]}")
+        # ServiceRequest.find(:all, :conditions => ['created_at between ? and ?', "#{params[:start_date]}" ,"#{params[:finish_date]}"])
         @received_params = "Nothing received"
       end
     else
-      @service_requests = ServiceRequest.all
+      @service_requests = ServiceRequest.where("status = ?", 1) #ServiceRequest.all
       @received_params = "Nothing received"
     end
     
